c77c135051a0f9768962a163ceb79ef804ed31b3
Italoac <123761315+Italoac@users.noreply.github.com>
2023-01-27 14:23:53 -0300

    Opção de saída
    Incluso a opção de saída do sistema

--- Cartorio.c
+++ Cartorio.c
@@ -139,6 +139,7 @@ int main()
 		printf("\t1 - Registrar nomes\n");
 		printf("\t2 - Consultar nomes\n");
 		printf("\t3 - Deletar nomes\n\n");
+		printf("\t4 - Sair do sistema\n\n");
 		printf("Digite a Opção: "); //Fim do menu
 		
 		scanf("%d", &opcao); //Comando entrada de valores na variavel
@@ -157,7 +158,12 @@ int main()
 			
 			case 3:
 			deletar();
-			break;			
+			break;
+			
+			case 4:
+			printf("Obrigado por utilizar o sistema!\n");
+			return 0;
+			break;		
 
 			default:
 			printf("Essa opção naõ está disponível! \n\n");
